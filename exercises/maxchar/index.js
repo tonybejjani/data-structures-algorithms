3f55cbe7de2d8afbf56dd4be759057f69c0897a0
Tony <bejjani.tony@gmail.com>
2022-07-11 17:30:56 +1000

    solved section 3,4,5,6

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // --- Directions
 // Given a string, return the character that is most
 // commonly used in the string.
@@ -5,6 +7,27 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let objStr = {};
+  let maxChar = 0;
+  let result = str[0];
+
+  for (char of str) {
+    if (objStr[char]) {
+      objStr[char] += 1;
+
+      if (objStr[char] > maxChar) {
+        maxChar = objStr[char];
+        result = char;
+      }
+    } else {
+      objStr[char] = 1;
+    }
+  }
+
+  return result;
+}
+
+maxChar('apple 1231111');
 
 module.exports = maxChar;
