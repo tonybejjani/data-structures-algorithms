c80e3c592b51c9558e4afe4e7b22bcba844befe7
Tony <bejjani.tony@gmail.com>
2022-07-11 17:33:03 +1000

    staging changes not applied on last commit

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // --- Directions
 // Given an integer, return an integer that is the reverse
 // ordering of numbers.
@@ -8,6 +10,22 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let nString = n.toString();
+  let negativeNum = nString.includes('-');
+  if (negativeNum) nString = nString.slice(1);
+
+  let reversed = nString.split('').reduce((prev, curr, i) => {
+    let next = nString[i + 1];
+    if (next === '0' && !nString[i + 2]) {
+      return prev;
+    } else {
+      return curr + prev;
+    }
+  });
+
+  if (negativeNum) return Number(reversed) * -1;
+  return Number(reversed);
+}
 
 module.exports = reverseInt;
--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -1,11 +1,17 @@
+/** @format */
+
 // --- Directions
 // Given a string, return a new string with the reversed
 // order of characters
 // --- Examples
-//   reverse('apple') === 'leppa'
+//   reverse('apple') === 'elppa'
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str.split('').reduce((prev, cur) => cur + prev);
+}
+
+// const newstr = reverse('tony');
 
 module.exports = reverse;
