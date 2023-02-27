25b1bc0bd29f5367cd830b5b36cb22feba5e65be
Tony <bejjani.tony@gmail.com>
2023-02-28 09:16:34 +1100

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -1,3 +1,5 @@
+/** @format */
+
 // --- Directions
 // Given an array and chunk size, divide the array into many subarrays
 // where each subarray is of length size
@@ -8,6 +10,27 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(arr, size) {
+  if (size >= arr.length) return arr;
+
+  const chunked = [];
+  const arrCopy = arr.map((el) => el);
+  const remainder = arr.length % size;
+  const loopCount = (arr.length - remainder) / size;
+
+  for (let i = 0; i < loopCount; i++) {
+    let chunk = arrCopy.splice(0, size);
+    chunked.push(chunk);
+    debugger;
+  }
+
+  if (remainder !== 0) {
+    chunked.push(arrCopy);
+  }
+
+  return chunked;
+}
+
+chunk([1, 2, 3, 4, 5], 2);
 
 module.exports = chunk;
--- exercises/script.js
+++ exercises/script.js
@@ -3,13 +3,29 @@
 
 // 0 + 1 + 2 + 3 + 4
 
-const testFunc = function (n) {
-  for (i = 1; i <= n; i++) {
-    if (i % 3 === 0 && i % 5 === 0) console.log('fizzbuzz');
-    else if (i % 3 === 0) console.log('fizz');
-    else if (i % 5 === 0) console.log('buzz');
-    else console.log(i);
+const arr = [1, 2, 3];
+const size = 1;
+
+const chunk = function (arr, size) {
+  if (size >= arr.length) return [arr];
+
+  const chunkedArr = [];
+  const arrCopy = arr.map((el) => el);
+  const remainder = arr.length % size;
+
+  for (let i = 0; i < (arr.length - remainder) / size; i++) {
+    chunkedArr.push(arrCopy.splice(0, size));
   }
+
+  if (remainder !== 0) {
+    chunkedArr.push(arrCopy);
+  }
+
+  return [chunkedArr];
 };
 
-// expected output: 10
+const result = chunk(arr, size);
+
+console.log(result);
+
+// console.log(newArr);
